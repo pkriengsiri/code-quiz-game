609d909e5f7a5f58b5b4e4041aa5c355323ff415
Pete Kriengsiri <pkriengsiri@gmail.com>
2020-12-12 14:52:27 -0500

    Added function to display scores on highscore page.  Updated page colors

--- assets/scripts/scores.js
+++ assets/scripts/scores.js
@@ -16,11 +16,28 @@ function displayScores() {
     scoresArray = JSON.parse(localStorage.getItem("saved-scores"));
     scoresArray.sort();
     
-    
+    for(var i = 0; i < scoresArray.length; i++) {
+        var liEl = document.createElement("li");
+        var pair = scoresArray[i].split(" ");
+        console.log(scoresArray[i]);
+        console.log(pair);
+        liEl.textContent = pair[1]+" - "+pair[0];
+        liEl.classList.add("list-group-item");
+        liEl.classList.add("list-group-item-action");
+        liEl.classList.add("list-group-item-primary");
+        console.log(liEl);
+        scoreListEl.append(liEl);
+    }
+}
+
+function resetScores () {
+    // Clear localStorage
+    storage.clear();
+
 }
 
 // Function calls
 displayScores();
 
 // Event handlers
-
+goBackButtonEl.addEventListener("click",returnToStart);
