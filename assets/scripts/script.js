b3629477e51d222c9f9f4a1a4c5dfe2af07eeda0
Pete Kriengsiri <pkriengsiri@gmail.com>
2020-12-11 20:46:28 -0500

    Separated starting content from quiz content in html.  Added all quiz question objects.  Started on function to process the quiz question answer

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,25 +1,50 @@
 // DOM Variables
 var startButtonEl = document.getElementById("start-button");
+var startContentEl = document.getElementById("start-content");
 var quizContentEl = document.getElementById("quiz-content");
 
 // JS Variables
+//quiz questions
 var quizQuestion1 = {
   question: "Commonly used data types DO NOT include",
   answers: ["strings", "booleans", "alerts", "numbers"],
   correctAnswer: "alerts",
 };
 
-var questionsArray = [quizQuestion1]; // array of quiz Questions
+var quizQuestion2 = {
+    question: "The condition in an if/ else statement is enclosed within ______.",
+    answers: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    correctAnswer: "quotes",
+  };
+
+  var quizQuestion3 = {
+    question: "Arrays in JavaScript can be used to store ______.",
+    answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    correctAnswer: "all of the above",
+  };
+
+  var quizQuestion4 = {
+    question: "String values must be enclosed within ______ when being assigned to variables.",
+    answers: ["commas", "curly brackets", "quotes", "parenthesis"],
+    correctAnswer: "quotes",
+  };
+
+  var quizQuestion5 = {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: ["JavaScript", "terminal/ bash", "for loops", "console log"],
+    correctAnswer: "console log",
+  };
+
+var questionsArray = [quizQuestion1,quizQuestion2,quizQuestion3,quizQuestion4,quizQuestion5]; // array of quiz Questions
+var questionIndex = 0; //tracks the current question
 
 // Function Definitions
 
 function startQuiz() {
-  // Clear the box
-  quizContentEl.innerHTML = "";
+  // Clear the box and remove padding
+  startContentEl.innerHTML = "";
+  startContentEl.classList.remove("p-3");
   // Change center-aligned text to right-aligned
-  quizContentEl.classList.remove("text-center");
-  quizContentEl.classList.add("text-right");
-  quizContentEl.classList.add("p-5");
   showQuizQuestion(0);
 }
 
@@ -32,27 +57,49 @@ function showQuizQuestion(index) {
   h1El.innerHTML = questionsArray[index].question;
   quizContentEl.append(h1El);
   
-  //Create ol
-  var olEl = document.createElement("ol");
-  quizContentEl.append(olEl);
+  //Create ul
+  var ulEl = document.createElement("ul");
+  ulEl.style = "list-style-type:none";
+  quizContentEl.append(ulEl);
   
   //Create li for each question answer
   for (var i = 0; i < questionsArray[index].answers.length; i++) {
     //create li
     var liEl = document.createElement("li");
-    olEl.append(liEl);
+    liEl.classList.add("p-1");
+    ulEl.append(liEl);
 
     //Create button
     var buttonEl = document.createElement("button");
-    buttonEl.textContent = questionsArray[index].answers[i];
+    var questionNumber = i+1;
+    buttonEl.textContent = questionNumber+". ";
+    var spanEl = document.createElement("span");
+    spanEl.textContent = questionsArray[index].answers[i];
     liEl.append(buttonEl);
+    buttonEl.append(spanEl);
   }
 }
 
-function processQuizAnswer() {}
+function processQuizAnswer(event) {
+    if (event.target.matches("button")) {
+        console.log("Clicking a quiz button");
+        // Condition to check if this is the last question
+        if(questionIndex === questionsArray.length -1) {
+            // process the answer
+            // go to the final score tally
+            alert("end of quiz");
+        } else {
+            //process the answer
+            //display the next question
+            questionIndex++;
+            showQuizQuestion(questionIndex);
+        }
+    }
+}
 
 // Function Calls
 
 // Event listeners
 
 startButtonEl.addEventListener("click", startQuiz);
+quizContentEl.addEventListener("click",processQuizAnswer);
